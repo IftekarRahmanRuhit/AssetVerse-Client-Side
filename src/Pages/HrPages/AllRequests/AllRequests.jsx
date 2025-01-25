3218c0d1b673c9a4b0649fe5f176d59030275d43
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2025-01-26 00:19:20 +0600

    added design for All request page

--- src/Pages/HrPages/AllRequests/AllRequests.jsx
+++ src/Pages/HrPages/AllRequests/AllRequests.jsx
@@ -57,7 +57,7 @@ const AllRequests = () => {
           onClick={() => handlePageClick(i)}
           className={`mx-1 px-3 py-1 rounded ${
             currentPage === i 
-              ? 'bg-blue-500 text-white' 
+              ? 'bg-[#9538E2] text-white' 
               : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
           }`}
         >
@@ -81,9 +81,9 @@ const AllRequests = () => {
   return (
     <section className='container px-4 mx-auto my-12'>
        <Helmet> <title>AssetVerse | All Requests</title> </Helmet>
-      <div className='flex flex-col md:flex-row md:items-center justify-between gap-4 mb-6'>
-        <div className='flex items-center gap-x-3'>
-          <h2 className='text-lg font-medium text-gray-800'>Asset Requests</h2>
+      <div className='flex flex-col md:flex-row md:items-center justify-between gap-4 mb-6 mt-36'>
+        <div className='flex text-center items-center gap-x-3'>
+          <h2 className='text-2xl font-medium text-[#9538E2]'>Asset Requests</h2>
           <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full'>
             {requests.length} Requests
           </span>
@@ -94,7 +94,7 @@ const AllRequests = () => {
           placeholder="Search by name or email..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full md:w-64 p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+          className="w-full md:w-64 p-2 rounded text-black bg-gray-200 border border-gray-300 focus:outline-none focus:border-[#9538E2]"
         />
       </div>
 
@@ -103,27 +103,27 @@ const AllRequests = () => {
           <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
             <div className='overflow-hidden border border-gray-200 md:rounded-lg'>
               <table className='min-w-full divide-y divide-gray-200'>
-                <thead className='bg-gray-50'>
+                <thead className='bg-gray-300'>
                   <tr>
-                    <th className='py-3.5 px-4 text-sm font-normal text-left text-gray-500'>
+                    <th className='py-3.5 px-4 text-sm font-bold text-center text-gray-800 '>
                       Asset Name
                     </th>
-                    <th className='py-3.5 px-4 text-sm font-normal text-left text-gray-500'>
+                    <th className='py-3.5 px-4 text-sm font-bold text-center text-gray-800 '>
                       Type
                     </th>
-                    <th className='py-3.5 px-4 text-sm font-normal text-left text-gray-500'>
+                    <th className='py-3.5 px-4 text-sm font-bold text-center text-gray-800 '>
                       Requester
                     </th>
-                    <th className='px-4 py-3.5 text-sm font-normal text-left text-gray-500'>
+                    <th className='px-4 py-3.5 text-sm font-bold text-center text-gray-800 '>
                       Request Date
                     </th>
-                    <th className='px-4 py-3.5 text-sm font-normal text-left text-gray-500'>
+                    <th className='px-4 py-3.5 text-sm font-bold text-center text-gray-800 '>
                       Note
                     </th>
-                    <th className='px-4 py-3.5 text-sm font-normal text-left text-gray-500'>
+                    <th className='px-4 py-3.5 text-sm font-bold text-center text-gray-800 '>
                       Status
                     </th>
-                    <th className='px-4 py-3.5 text-sm font-normal text-left text-gray-500'>
+                    <th className='px-4 py-3.5 text-sm font-bold text-center text-gray-800 '>
                       Actions
                     </th>
                   </tr>
@@ -131,10 +131,10 @@ const AllRequests = () => {
                 <tbody className='bg-white divide-y divide-gray-200'>
                   {currentRequests.map((request) => (
                     <tr key={request._id}>
-                      <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>
+                      <td className='px-4 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap'>
                         {request.assetName}
                       </td>
-                      <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>
+                      <td className='px-4 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap'>
                         <span className={`px-3 py-1 rounded-full text-xs
                           ${request.assetType === 'Returnable' 
                             ? 'bg-blue-100/60 text-blue-500' 
@@ -142,19 +142,19 @@ const AllRequests = () => {
                           {request.assetType}
                         </span>
                       </td>
-                      <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>
+                      <td className='px-4 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap'>
                         <div>
                           <p>{request.requesterName}</p>
                           <p className="text-xs text-gray-400">{request.requesterEmail}</p>
                         </div>
                       </td>
-                      <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>
+                      <td className='px-4 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap'>
                         {format(new Date(request.requestDate), 'PP')}
                       </td>
-                      <td className='px-4 py-4 text-sm text-gray-500 max-w-xs truncate'>
+                      <td className='px-4 py-4 text-sm font-medium text-center text-gray-800 max-w-xs truncate'>
                         {request.additionalNotes}
                       </td>
-                      <td className='px-4 py-4 text-sm whitespace-nowrap'>
+                      <td className='px-4 py-4 text-sm text-center whitespace-nowrap'>
                         <div className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2
                           ${request.status === 'pending' ? 'bg-yellow-100/60 text-yellow-500' :
                             request.status === 'approved' ? 'bg-green-100/60 text-green-500' :
@@ -165,11 +165,11 @@ const AllRequests = () => {
                         </div>
                       </td>
                       <td className='px-4 py-4 text-sm whitespace-nowrap'>
-                        <div className='flex items-center gap-x-4'>
+                        <div className='flex items-center gap-x-4 '>
                           <button
                             disabled={request.status !== 'pending'}
                             onClick={() => handleStatusChange(request._id, 'approved')}
-                            className='disabled:opacity-50 disabled:cursor-not-allowed text-gray-500 transition-colors duration-200 hover:text-green-500 focus:outline-none'
+                            className='disabled:opacity-50 disabled:cursor-not-allowed text-gray-800 transition-colors duration-200 hover:text-green-500 focus:outline-none'
                           >
                             <svg
                               xmlns='http://www.w3.org/2000/svg'
@@ -190,7 +190,7 @@ const AllRequests = () => {
                           <button
                             disabled={request.status !== 'pending'}
                             onClick={() => handleStatusChange(request._id, 'rejected')}
-                            className='disabled:opacity-50 disabled:cursor-not-allowed text-gray-500 transition-colors duration-200 hover:text-red-500 focus:outline-none'
+                            className='disabled:opacity-50 disabled:cursor-not-allowed text-gray-800 transition-colors duration-200 hover:text-red-500 focus:outline-none'
                           >
                             <svg
                               xmlns='http://www.w3.org/2000/svg'
